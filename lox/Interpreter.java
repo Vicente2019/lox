fb75074b3d6dcdd632dba4dc0fe8788284e139d4
Vicente Marques <vicente.marques@tecnico.ulisboa.pt>
2024-06-26 18:12:43 +0100

    get expressions in classes

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.naming.event.ObjectChangeListener;
+
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     
     final Environment globals = new Environment();
@@ -287,5 +289,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return function.call(this, arguments);
+    }
+
+    @Override
+    public Object visitGetExpr(Expr.Get expr) {
+        Object object = evaluate(expr.object);
+        if (object instanceof LoxInstance) {
+            return ((LoxInstance) object).get(expr.name);
+        }
+        throw new RuntimeError(expr.name, "Only instances have properties.");
     }
 }
